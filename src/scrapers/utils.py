563b34bc0d610d6acf8b7d8629fc40982d108977
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-09-20 21:39:50 +0600

    refactoring

--- src/scrapers/utils.py
+++ src/scrapers/utils.py
@@ -1,11 +1,10 @@
-from . import scrap_24kg, scrap_akipress, scrap_vesti
+from src.scrapers import 24kg
+from . import akipress, vesti, _24kg
 
 def parse():
-    result = {
-
-    }
-    result['akipress'] = scrap_akipress.ScrapAkipress.as_scraper()
-    result['24_kg'] = scrap_24kg.Scrap24kg.as_scraper()
-    result['vesti'] = scrap_vesti.ScraperVesti.as_scraper()
+    result = {}
+    result['akipress'] = akipress.ScrapAkipress.as_scraper()
+    result['24_kg'] = _24kg.Scrap24kg.as_scraper()
+    result['vesti'] = vesti.ScraperVesti.as_scraper()
 
     return result
